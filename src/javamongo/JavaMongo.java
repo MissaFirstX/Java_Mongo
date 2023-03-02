a87a8f3e98bd5951ef257a6098615bcc73e72aa8
MissaFirstX <araizamartinezmissael@gmail.com>
2023-03-01 18:51:20 -0600

    Commit 1

---
+++ src/javamongo/JavaMongo.java
@@ -0,0 +1,19 @@
+package javamongo;
+
+public class JavaMongo {
+
+    public static void main(String[] args) {
+        Connection conexion = new Connection();
+
+        conexion.Mostrar();
+        //conexion.insertar("Nadar");
+        //System.out.println("Otro");
+        //conexion.Mostrar();
+        //System.out.println("otro mas");
+        //conexion.Actualizar("Nadar","Brincar");
+        //conexion.eliminar("Brincar");
+        //conexion.Mostrar();
+
+    }
+
+}
